c4f4cf994ff62d3539524fdc7ddf820374d8c5a3
Derek Dunlap <derekdunlapjr@gmail.com>
2021-08-06 17:42:21 -0400

    merged navbar.css and added home.jpg deleted duplicate files

--- SportsNewsApp/app.js
+++ SportsNewsApp/app.js
@@ -1,38 +1,39 @@
-const sportsNewsUL=document.getElementById("sportsNewsUL")
-const nflNewsButton=document.getElementById("nflNewsButton")
+let mainListDiv = document.getElementById("mainListDiv"),
+    mediaButton = document.getElementById("mediaButton");
+const sportsUL=document.getElementById("sportsUL")
 
-//gets Sports News Articles from NewsAPI with LiveServer turned on. Pass url and callback function
-function getSportsNews(sportsNewsURL,sprtsNewsDownloaded){
-    fetch(sportsNewsURL)
+mediaButton.onclick = function () {
+    
+    "use strict";
+    
+    mainListDiv.classList.toggle("show_list");
+    mediaButton.classList.toggle("active");
+    
+};
+
+function getSportsData(sportsDataDownloaded){
+    const sportsURL=``
+    fetch(sportsURL)
     .then(response=>{
         return response.json()
-    }).then(sportsNewsArticles=>{
-        console.log(sportsNewsArticles)
-        sprtsNewsDownloaded(sportsNewsArticles)
-        
+    }).then(sportsData=>{
+        sportsDataDownloaded(sportsData)
     })
 }
 
-//addEventListener for NFL Tab Button
-nflNewsButton.addEventListener('click',function(){
-    const nflURL=`https://newsapi.org/v2/top-headlines?q=nfl&apiKey=e2f529560d044928b22b33b3c8282dfe`
-    getSportsNews(nflNewsURL,function(sportsNewArticles){
-        displaySportsArticles(sportsNewArticles)
-    })
+getSportsData(function(sportsData){
+    displaySportsArticles(sportsData)
 })
 
-//display function can be called by all sports tab (Examples: NFL,NBA,Soccer)
-function displaySportsArticles(sportsNewArticles){
-    const sportsArticles=sportsNewArticles.articles
-    const sportItem=sportsArticles.map(function(sportArticle){
-        const sportsNewsTemplate=`<li>  
-            <h2>${sportArticle.title}</h2>
-            <img src="${sportArticle.urlToImage}"/>
-            <span>Caption ${sportArticle.description}</span>
-            <p>${sportArticle.content}</p>
+function displaySportsArticles(sportsArticles){
+    const sportsArr=sportsArticles.data
+    const sportsItem=sportsArr.map(function(sportArticle){
+        const sportTemplate=`<li>  
+            <h2></h2>
         </li>
         `
-        return sportsNewsTemplate
+        return sportTemplate
     })
-    sportsNewsUL.innerHTML=sportItem.join("")
+    sportsUL.innerHTML=sportsItem.join("")
 }
+
