f4d7e166ef84f8d99a901d876ed24ffa5608b964
Derek Dunlap <derekdunlapjr@gmail.com>
2021-08-06 16:17:14 -0400

    added functionality to fetch sportsNewsArticles and Display Articles. Still need to fully implement NFL Button feature.

--- SportsNewsApp/app.js
+++ SportsNewsApp/app.js
@@ -1,27 +1,38 @@
-const sportsUL=document.getElementById("sportsUL")
+const sportsNewsUL=document.getElementById("sportsNewsUL")
+const nflNewsButton=document.getElementById("nflNewsButton")
 
-function getSportsData(sportsDataDownloaded){
-    const sportsURL=``
-    fetch(sportsURL)
+//gets Sports News Articles from NewsAPI with LiveServer turned on. Pass url and callback function
+function getSportsNews(sportsNewsURL,sprtsNewsDownloaded){
+    fetch(sportsNewsURL)
     .then(response=>{
         return response.json()
-    }).then(sportsData=>{
-        sportsDataDownloaded(sportsData)
+    }).then(sportsNewsArticles=>{
+        console.log(sportsNewsArticles)
+        sprtsNewsDownloaded(sportsNewsArticles)
+        
     })
 }
 
-getSportsData(function(sportsData){
-    displaySportsArticles(sportsData)
+//addEventListener for NFL Tab Button
+nflNewsButton.addEventListener('click',function(){
+    const nflURL=`https://newsapi.org/v2/top-headlines?q=nfl&apiKey=e2f529560d044928b22b33b3c8282dfe`
+    getSportsNews(nflNewsURL,function(sportsNewArticles){
+        displaySportsArticles(sportsNewArticles)
+    })
 })
 
-function displaySportsArticles(sportsArticles){
-    const sportsArr=sportsArticles.data
-    const sportsItem=sportsArr.map(function(sportArticle){
-        const sportTemplate=`<li>  
-            <h2></h2>
+//display function can be called by all sports tab (Examples: NFL,NBA,Soccer)
+function displaySportsArticles(sportsNewArticles){
+    const sportsArticles=sportsNewArticles.articles
+    const sportItem=sportsArticles.map(function(sportArticle){
+        const sportsNewsTemplate=`<li>  
+            <h2>${sportArticle.title}</h2>
+            <img src="${sportArticle.urlToImage}"/>
+            <span>Caption ${sportArticle.description}</span>
+            <p>${sportArticle.content}</p>
         </li>
         `
-        return sportTemplate
+        return sportsNewsTemplate
     })
-    sportsUL.innerHTML=sportsItem.join("")
+    sportsNewsUL.innerHTML=sportItem.join("")
 }
